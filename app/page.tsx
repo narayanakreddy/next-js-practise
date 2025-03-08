4bcaa43ad6eabe8b01a28ba43d8fb2bcc2302120
Narayana Reddy <narayanak1515@gmail.com>
2025-03-08 17:05:20 +0530

    latest changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,72 @@
-import Image from "next/image";
+"use client";
+import { Button, Form, Input } from "antd";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="login-container height">
+      <div className="row height">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 d-none d-lg-block">
+          layout
+        </div>
+        <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+          <div className="d-flex flex-column height  justify-content-center">
+            <p>test data</p>
+            <Form layout="vertical">
+              <div style={{ position: "relative" }}>
+                <Form.Item
+                  label="Username"
+                  name="username"
+                  rules={[{ required: true, message: "Required Field" }]}
+                >
+                  <Input size="large" placeholder="Username" />
+                </Form.Item>
+              </div>
+              <div style={{ position: "relative" }}>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[{ required: true, message: "Required Field" }]}
+                >
+                  <Input.Password
+                    className="login-password"
+                    maxLength={20}
+                    size="large"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+              </div>
 
-        
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+              {/* <span
+          className="text-end d-block auth-link-style"
+          style={{ marginTop: "-10px" }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Forgot Password?
+        </span> */}
+
+              <Button
+                style={{ marginLeft: 0 }}
+                className="login-button mt-2 mb-3"
+                block
+                size="large"
+                type="primary"
+                htmlType="submit"
+              >
+                Sign In
+              </Button>
+
+              {/* <span className="text-center d-block">
+          Not You?{" "}
+          <span
+            className="auth-link-style"
+            style={{ textDecoration: "underline" }}
+          >
+            Login using different email address
+          </span>
+        </span> */}
+            </Form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
